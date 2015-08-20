fb7b5d8d3e628d224c9140bcbbbf7ca6d050db59
tdwhite0 <Rpazuredeploy>
2015-08-20 15:09:45 -0400

    use the login endpoint directly

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var BASE_LOGIN_URL = "https://login.microsoftonline.com/login.srf?wa=wsignin1.0&wreply=";
+
 module.exports = {
 
     loginToOffice365: function(username, password, siteUrl, loginMethod) {
 
         if (loginMethod === "integrated" || typeof(loginMethod) === "undefined") {
-            return this.url(siteUrl)
+            return this.url(BASE_LOGIN_URL + siteUrl)
                 .waitForExist("#cred_sign_in_button")
                 .click('#cred_userid_inputtext')
                 .keys(username)
@@ -14,7 +16,7 @@ module.exports = {
 
         if (loginMethod === "adfs") {
 
-            return this.url(siteUrl)
+            return this.url(BASE_LOGIN_URL + siteUrl)
                 .waitForExist("#submitButton")
                 .click('#userNameInput')
                 .keys(username)
